1aa1ede5cbe8eaa0bda2c9d255592823d4ed5de0
EnJag <enzo.jag01@gmail.com>
2024-12-16 11:17:02 +0100

    ajout nation & formations

--- Overview.py
+++ Overview.py
@@ -1,4 +1,4 @@
-from Utils import load_credentials,perfomance_globale_joueurs,meilleur_ami,classement_coachs,compet_favoris_par_nation,men_vs_women,service_and_meteo,perf_tournoi_majeurs,perf_by_age,evolution_age,perf_by_ranking,meilleur_ennemi,analyse_blessures,stats_finales
+from Utils import load_credentials,perfomance_globale_joueurs,meilleur_ami,classement_coachs,compet_favoris_par_nation,men_vs_women,service_and_meteo,perf_tournoi_majeurs,perf_by_age,evolution_age,perf_by_ranking,meilleur_ennemi,analyse_blessures,stats_finales, nation_formation
 from Database import Database
 import streamlit as st
 import pandas as pd
@@ -112,3 +112,6 @@ elif option == "Profil Météo":
 # Men vs Women
 elif option == "Men vs Women":
     men_vs_women()
+
+elif option == "Nation & Formation":
+    nation_formation()
